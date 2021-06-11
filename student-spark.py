c3ce38fd7a180e742009139c2cbf4f9d9a193124
deepaksinghea <deepak.singh.ea@gmail.com>
2021-06-11 19:39:06 +0530

    print schema

--- student-spark.py
+++ student-spark.py
@@ -5,8 +5,10 @@ sc = SparkContext(master = 'local[*]')
 
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.master("local[*]").appName("Student Pyspark Example").getOrCreate()
+spark = SparkSession.builder.master("local[*]")\
+                    .appName("Student Pyspark Example")\
+                    .getOrCreate()
 
 df = spark.read.option("header",True).csv('/Users/deepak/Downloads/sample.csv')
 
-df.show()
+df.printSchema()
